6fefe7979d3a07354ac81a63af4026812a21cb68
Lizazal <liza.rzal@gmail.com>
2025-10-15 18:46:48 +0300

    index.html update: date%time added

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template, request, redirect, url_for
+from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, 'ru_RU')
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    current_time = datetime.now()
+    return render_template('index.html', current_time=current_time)
 
 
 @app.route('/about')
